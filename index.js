697e3f47a5177da8c778c5d43c4cd1992d74384b
mizuki-yamada <yamada.mizuki.0730@gmail.com>
2023-05-31 22:05:01 +0900

    アニメーションを追加

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 "use strict";
 {
-    const open = document.getElementById("open");
-    const close = document.getElementById("close");
-    const modal = document.getElementById("modal");
-    const background = document.getElementById("background");
-    
+  const open = document.getElementById("open");
+  const close = document.getElementById("close");
+  const modal = document.getElementById("modal");
+  const background = document.getElementById("background");
+
   open.addEventListener("click", () => {
-      modal.classList.remove('hidden');
-      background.classList.remove('hidden');
+    modal.classList.remove("hidden");
+    background.classList.remove("hidden");
+  });
+
+  close.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    background.classList.add("hidden");
+  });
+
+  background.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    background.classList.add("hidden");
   });
 }
